503c414fec9ad76a496c92a52fb8412993a43adc
William Trindade <williamtrindade777@gmail.com>
2018-05-22 15:54:21 -0300

    Create script.js

---
+++ Aula-Javascript/01/script.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html >
+
+<html lang="pt">
+
+	<head>
+		 <meta charset="utf-8" />
+		 <title>JavaScript - Testes</title>
+	</head>
+
+	<body>
+		<p>Usando JavaScript</p>
+		<script>
+
+			alert("Olá Mundo!");
+
+		</script>
+	</body>
+
+</html>
