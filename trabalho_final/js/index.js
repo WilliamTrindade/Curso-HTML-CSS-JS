ae11840504f6d81fbeb756cf50a1a4f1bbb39259
William Trindade <williamtrindade777@gmail.com>
2018-07-03 00:06:39 -0300

    Add files via upload

---
+++ trabalho_final/js/index.js
@@ -0,0 +1,22 @@
+window.onload = function inicio(){
+    //footer copyright
+    var d = new Date;
+    if( d.getFullYear() == 2017){ //Here are the year in what you company start
+        document.getElementById("copy").innerHTML = "© 2017 - TION SOFTWARES.Inc - Todos os direitos reservados.";//copyright text
+    }else{
+        document.getElementById("copy").innerHTML = "© 2017 - " + d.getFullYear() + " - TION SOFTWARES.Inc - Todos os direitos reservados.";//copyright text
+    }
+}
+//navbar
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function valida(){
+    alert('Parabéns agora você vai receber novidades do mundo do software no seu e-mail');
+}
